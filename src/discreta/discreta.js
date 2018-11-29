156d0402fffdd3b09bfe8507067ae24ec938e2e8
João P. M. O. Silva <jpaulo.motta@gmail.com>
2018-11-29 12:27:30 -0200

    Adciona discreta

---
+++ src/discreta/discreta.js
@@ -0,0 +1,107 @@
+
+var f = [];
+function fatorial (n) {
+  if (n == 0 || n == 1)
+    return 1;
+  if (f[n] > 0)
+    return f[n];
+  return f[n] = fatorial(n-1) * n;
+}
+
+function combinacao(n, p) {
+    return fatorial(n)/(fatorial(p) * fatorial( n - p ))
+}
+
+/** Implementando segundo sesão 3.4 */
+function permutacaoRepeticao(n, rVet=[]) {
+    if(rVet.length === 0) {
+        return 0
+    }
+    if(rVet.reduce((memo, r) => memo+r) !== n) {
+        throw new Error('O somatório de rVet deve ser = n')
+    }
+    let denominador = 1
+    for(let r of rVet) {
+        denominador *= fatorial(r)
+    }
+    let resultado = fatorial(n)/denominador
+    return resultado
+}
+
+/**
+ * Calcula a quantidade de distintos anagramas em um texto com as seguintes restrições
+ * "As permutações devem ser tais que os espaços não ocorrem subseqüentes (i.e., seguidos) 
+ * um de outro, e nem nas extremidades do anagrama."
+ * 
+ * Explicação:
+ * O algoritmo foi elaborado baseando-se no principio da inclusão e exclusão.
+ * Temos os conjuntos
+ * total :=  total de anagramas possiveis, sem restrições
+ * a1 := anagramas sem espaços repetidos
+ * a2 := anagramas sem espaços no inicio do texto
+ * a3 := anagramas sem espaços no fim do texto
+ * s := quantidade de espaços no texto
+ * 
+ * A resposta para o problema pode ser encontrada atraves de
+ * |total| - |a1 U a2 U a3|
+ * => |total| - |a1| - |a2| - |a3| + |a1 ∩ a2| + |a1 ∩ a3| + |a2 ∩ a3| - |a1 ∩ a2 ∩ a3|
+ *  
+ * sendo:
+ *  n := numero de caracteres no texto
+ *  ri := quantidade do caractere i no texto, com i pertencendo a  {A, B, ..., Z} U {0, 1, 2, ..., 9}
+ * PR := permutacaoRepeticao(n, [r1, r2 ..., rn])
+ * |total| = PR(n, r1, ..., rn, s)
+ * |a1| = PR((n - s) + 1, r1, ..., rn, 1 ) se s >= 2
+ * |a2| = |a3| = PR((n-1), r1, ..., rn, s-1) se s >= 1
+ * |a1 ∩ a2| = |a1 ∩ a3| = Pr( (n-1) - (s-1) + 1, r1, ..., rn, 1), se s >= 2 
+ * ou
+ * |a1 ∩ a2| = |a1 ∩ a3| = Pr( (n-1) - (s-1), r1, ..., rn, 0), se s = 1
+ * |a1 ∩ a2 ∩ a3| = Pr((n-2) - (s-2) + 1, r1, ..., rn, 1) se se >= 3
+ * |a1 ∩ a2 ∩ a3| = Pr((n-2), r1, ..., rn, 1) se se = 2 
+ */
+function anagrama(texto) {
+    let quantidade = {' ': 0}
+    //Calcula quantidade de simbolos no texto
+    for(let simbolo of texto) {
+        if(!quantidade[simbolo]) {
+            quantidade[simbolo] = 1
+        } else {
+            quantidade[simbolo] += 1
+        }
+    }
+    console.log(quantidade)
+    //Calcula total de permutações possiveis
+    let n = texto.length
+    let nTotal = permutacaoRepeticao(n, Object.values(quantidade))
+    let s = quantidade[' ']
+    
+    if(s === 0) {
+        //Se não houver espaços no texto, retorne nTotal
+        return nTotal
+    } else if(s === 1) {
+        //se s = 1 então devemos calcular |total| - |a2 U a3| já que não existira repetição
+        //pelo principio inclusao e exclusao isso é 
+        // |total| - |a2| - |a3| + |a2 ∩ a3|
+        // e |a2 ∩ a3| = 0 pois não existem permutações com 1 espaço onde ele esteja no inicio E fim
+        // do texto
+        let a2 = permutacaoRepeticao(n-1, Object.values( {...quantidade, ' ': s-1} ))
+        let a3 = a2
+        return nTotal - a2 - a3 + 0
+    } else if(s >= 2) {
+        let a1 = permutacaoRepeticao((n - s) + 1, Object.values({...quantidade, ' ': 1}))
+        let a2 = permutacaoRepeticao(n-1, Object.values( {...quantidade, ' ': s-1} ))
+        let a3 = a2
+        let a1Ea2 = permutacaoRepeticao((n - 1) - (s-1) + 1, Object.values({...quantidade, ' ': 1}) )
+        let a1Ea3 = a1Ea2
+        let a2Ea3 = permutacaoRepeticao(n-2, Object.values({...quantidade, ' ': s-2}))
+        let a1Ea2Ea3
+        if(s === 2) {
+            a1Ea2Ea3 = permutacaoRepeticao(n-2, Object.values({...quantidade, ' ': 0}))
+        } else {
+            a1Ea2Ea3 = permutacaoRepeticao((n-2) - (s-2) + 1, Object.values({...quantidade, ' ': 1}))
+        }
+        return nTotal - a1 - a2 - a3 + a1Ea2 + a1Ea3 + a2Ea3 - a1Ea2Ea3
+    }
+}
+
+export {fatorial, combinacao, permutacaoRepeticao, anagrama}
---
+++ src/discreta/discreta.test.js
@@ -0,0 +1,29 @@
+import {fatorial, combinacao, permutacaoRepeticao, anagrama} from './discreta'
+
+test('fatorial', () => {
+    expect(fatorial(1)).toEqual(1)
+    expect(fatorial(2)).toEqual(2)
+    expect(fatorial(3)).toEqual(6)
+    expect(fatorial(10)).toEqual(3628800)
+})
+
+test('combinacao', () => {
+    //Capitulo 2, sesão 5
+    expect(combinacao(5, 3)).toEqual(10)
+    expect(combinacao(5, 5)).toEqual(1)
+})
+
+
+test('permutação com repetição', () => {
+    //Capitulo 3, Sesão 4
+    expect(permutacaoRepeticao(7, [3, 2, 2])).toEqual(210)
+})
+
+test('anamgrama', () => {
+    //Capitulo 3, Sesão 4
+    expect(anagrama('aaabbcc')).toEqual(210)
+
+    expect(anagrama('aabbccc ')).toEqual(1260)
+
+    expect(anagrama('FRASES COM 20 LETRAS')).toEqual(6046686277632000)
+})
